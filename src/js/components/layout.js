8f20c6f2bb1dc465236e2968b3a5dc6b316e1ac9
Esquilax76 <routierclement@gmail.com>
2018-04-20 14:25:24 +0200

    story page done

--- src/js/components/layout.js
+++ src/js/components/layout.js
@@ -1,58 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router";
-// import LogoSYSTRA from "../../img/logoSYSTRA.png";
-// import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-// import faFacebookSquare from '@fortawesome/fontawesome-free-brands/faFacebookSquare';
-// import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
-
-// class Header extends React.Component {
-//     render() {
-//         return (
-//             <header className="header">
-//                 <div className="headerTitle">LA BANOU</div>
-//                 <nav class="nav">
-//                     <Link to="bieres" className="navItem">NOS BIERES</Link>
-//                     <Link to="lieux" className="navItem">LES TROUVER</Link>
-//                     <Link to="histoire" className="navItem">NOTRE HISTOIRE</Link>
-//                     <Link to="contact" className="navItem">CONTACT</Link>
-//                 </nav>
-//             </header>
-//         );
-//     }
-// }
-
-// class Footer extends React.Component {
-//     render() {
-//         return (
-//             <footer className="footer">
-//                 <div>© 2017 LA BANOU</div>
-//                 <div className="links">
-//                     <a href="https://www.facebook.com/bierelabanou" target="blank">
-//                         <FontAwesomeIcon icon={faFacebookSquare} className="linkIcon"/>
-//                     </a>
-//                     <a href="https://www.instagram.com/biere_la_banou/?hl=fr" target="blank">
-//                         <FontAwesomeIcon icon={faInstagram} className="linkIcon"/>
-//                     </a>
-//                 </div>
-//             </footer>
-//         );
-//     }
-// }
-
-// class Layout extends React.Component {
-//     render() {
-//         return (
-//             <main>
-//                 <Header/>
-//                 {this.props.children}
-//                 <Footer/>
-//             </main>
-//         );
-//     }
-// }
-
-// export default Layout;
-
 import React from "react";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 import faFacebookSquare from '@fortawesome/fontawesome-free-brands/faFacebookSquare';
@@ -102,7 +47,7 @@ class Layout extends React.Component {
     render() {
         let options = {
             activeClass: "active", // the class that is appended to the sections links
-            anchors: ["bieres","histoire"], // the anchors for each sections
+            anchors: ["bieres", "histoire"], // the anchors for each sections
             arrowNavigation: true, // use arrow keys
             className: "SectionContainer", // the class name for the section container
             delay: 1000, // the scroll animation speed
@@ -117,7 +62,7 @@ class Layout extends React.Component {
         return (
             <main>
                 <Header/>
-                <SectionsContainer className="test" {...options}>
+                <SectionsContainer className="sectionContainer" {...options}>
                     <Beers/>
                     <Story/>
                 </SectionsContainer>
